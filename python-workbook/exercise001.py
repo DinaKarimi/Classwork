b26597c56c7f11f4d169464b8ad4b023195c8b89
DinaKarimi <47459183+DinaKarimi@users.noreply.github.com>
2019-02-13 14:07:38 -0500

    Create exercise001.py

---
+++ python-workbook/exercise001.py
@@ -0,0 +1,12 @@
+name = 'John Jones'
+address= '1234 RedSwan Rd.'
+city= 'Montreal'
+province= 'QC'
+postal_code = 'H3Z 2Y7'
+telephone='(236)339-842'
+print(name)
+print(address)
+print(f'{city},{province}')
+#print('{},{}'.format(city, province))
+print(postal_code)
+print(telephone)
