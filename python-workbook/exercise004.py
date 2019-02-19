9ebed2f8c094c3304a1da3f85b6a68edea387434
DinaKarimi <47459183+DinaKarimi@users.noreply.github.com>
2019-02-19 12:10:33 -0500

    Update exercise004.py

--- python-workbook/exercise004.py
+++ python-workbook/exercise004.py
@@ -1,4 +1,4 @@
-l= float(input('Enter length in feet:'))
-w=float(input('Enter width in feet:'))
+l= int(input('Enter length in feet:'))
+w= int(input('Enter width in feet:'))
 area = round(((l*w)/43560),2)
 print(f'The area of the field in acres is:{a}')
