433a6eb830bc9518dd86425462acbc4b02a0702a
DinaKarimi <47459183+DinaKarimi@users.noreply.github.com>
2019-02-28 13:51:57 -0500

    Update exercise036.py

--- python-workbook/exercise036.py
+++ python-workbook/exercise036.py
@@ -1,1 +1,8 @@
 
+word= input('letter of the alphabet:')
+if word=='o' or word=='i' or word=='a' or word=='u' or word=='e':
+    print('vowel')
+elif word=='y':
+    print('sometimes vowel,sometimes consonant')
+else:
+    print('consonant')
