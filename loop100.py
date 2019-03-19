fab3bdccac1faa717ea5721ed2d6bd59c689e4a5
DinaKarimi <47459183+DinaKarimi@users.noreply.github.com>
2019-03-19 13:19:02 -0400

    Create loop100.py

---
+++ loop100.py
@@ -0,0 +1,19 @@
+#1
+word= 'hello'
+for let in word:
+    if let !='o'and let!='e' :
+        print(let)
+
+#2
+nums=(3,5,11)
+for num in nums:
+    if num>10 :
+        print(num)
+        
+#3
+max=0
+nums=(1,2,3,4,143,154,678)
+for num in nums:
+  if num>max:
+    
+ print(num)
